43cdc59dcb8e6080995ce349051d37e796bc2896
maualkla <mauricio.alcala.4@gmail.com>
2018-12-19 10:05:16 -0600

    sin_sort

--- app/models/zip.rb
+++ app/models/zip.rb
@@ -44,23 +44,17 @@ class Zip
   #   * sort - hash expressing multi-term sort order
   #   * offset - document to start results
   #   * limit - number of documents to include
-  def self.all(prototype={}, sort={:population=>1}, offset||=0, limit||=50)
+  def self.all(prototype={}, offset=0, limit=50)
     #map internal :population term to :pop document term
     tmp = {} #hash needs to stay in stable order provided
-    sort.each {|k,v| 
-      k = k.to_sym==:population ? :pop : k.to_sym
-      tmp[k] = v  if [:city, :state, :pop].include?(k)
-    }
-    sort=tmp
 
     #convert to keys and then eliminate any properties not of interest
     prototype=prototype.symbolize_keys.slice(:city, :state) if !prototype.nil?
 
-    Rails.logger.debug {"getting all zips, prototype=#{prototype}, sort=#{sort}, offset=#{offset}, limit=#{limit}"}
+    Rails.logger.debug {"getting all zips, prototype=#{prototype}, offset=#{offset}, limit=#{limit}"}
 
     result=collection.find(prototype)
           .projection({_id:true, city:true, state:true, pop:true})
-          .sort(sort)
           .skip(offset)
     result=result.limit(limit) if !limit.nil?
 
